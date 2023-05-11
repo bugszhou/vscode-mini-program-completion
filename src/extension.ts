45cbcd778d4e4d9b8c97ddabc75c3470b7e834f5
youzhao.zhou <youzhao.zhou@verystar.cn>
2023-05-11 11:52:29 +0800

    feat: 增加注释解析功能

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,7 @@ import { dirname, join } from "path";
 import { readFileSync } from "jsonfile";
 import * as vscode from "vscode";
 import { Project, SyntaxKind } from "ts-morph";
+import { parse } from "comment-parser";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -42,6 +43,12 @@ export function activate(context: vscode.ExtensionContext) {
 
         let props: string[] = [];
 
+        const computedPropsType: string[] = [];
+        const newProps: any[] = [];
+
+        const imports = jsContent.getImportDeclarations();
+        console.log(imports[0].getModuleSpecifierSourceFile());
+
         jsContent.forEachChild((item) => {
           if (item.asKind(SyntaxKind.ClassDeclaration)) {
             try {
@@ -52,21 +59,77 @@ export function activate(context: vscode.ExtensionContext) {
                   .getInitializer()
                   ?.asKindOrThrow(SyntaxKind.ObjectLiteralExpression)
                   ?.getProperties()
-                  .map((property) => property.getText()?.split(":")[0] || "")
+                  .map((property) => {
+                    computedPropsType.push(property?.getType()?.getText());
+                    return property.getText()?.split(":")[0] || "";
+                  })
                   .filter((property) => property) ?? [];
+
+              item
+                .asKindOrThrow(SyntaxKind.ClassDeclaration)
+                .getPropertyOrThrow("properties")
+                .getType()
+                .getApparentType()
+                .getProperties()
+                .forEach((property, index) => {
+                  const tempProperty = property
+                    .getValueDeclaration()
+                    ?.asKindOrThrow(SyntaxKind.PropertySignature);
+
+                  const comment =
+                    tempProperty
+                      ?.getLeadingCommentRanges?.()?.[0]
+                      ?.getText?.() || "";
+
+                  const parsed = parse(comment, { spacing: "preserve" });
+                  const commentObj = parsed[0];
+                  const tagStr = commentObj?.tags
+                    ?.map?.(
+                      (tag) =>
+                        `@${tag.tag} - ${tag.type} - ${
+                          tag.description || "无"
+                        }`,
+                    )
+                    ?.join("\n ");
+
+                  newProps.push({
+                    label: tempProperty?.getText()?.split(":")[0] || "",
+                    detail: `
+Properties属性
+类型：${computedPropsType[index] || ""}
+说明：
+ ${commentObj?.description || ""}
+ ${tagStr || ""}
+                    `,
+                    kind: vscode.CompletionItemKind.Property,
+                  });
+                });
+
+              // const comments = item
+              //   .asKindOrThrow(SyntaxKind.ClassDeclaration)
+              //   .getPropertyOrThrow("properties")
+              //   .getInitializerOrThrow()
+              //   .asKindOrThrow(SyntaxKind.ObjectLiteralExpression)
+              //   .getPropertyOrThrow("name")
+              //   .getLeadingCommentRanges();
+
+              // console.log("comments: ", comments.length);
+              // comments.forEach((item) => {
+              //   console.log(item.getText());
+              // });
+              // console.log(
+              //   item
+              //     .asKindOrThrow(SyntaxKind.ClassDeclaration)
+              //     .getPropertyOrThrow("properties")
+              //     .getTrailingCommentRanges(),
+              // );
             } catch (e) {
               console.error(e);
             }
           }
         });
 
-        return Promise.resolve(
-          props.map((item) => ({
-            label: item,
-            detail: "属性",
-            kind: vscode.CompletionItemKind.Property,
-          })),
-        );
+        return Promise.resolve(newProps);
       },
     },
     "<",
