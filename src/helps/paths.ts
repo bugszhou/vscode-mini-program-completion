50cb00a557b1d366450aaa5b68678d420aec5181
youzhao.zhou <youzhao.zhou@verystar.cn>
2023-08-05 12:24:14 +0800

    refactor: 增加兼容性

--- src/helps/paths.ts
+++ src/helps/paths.ts
@@ -1,5 +1,5 @@
 import * as vscode from "vscode";
 
 export function getRoot() {
-  return vscode.workspace.workspaceFolders?.[0].uri.fsPath || "";
+  return vscode.workspace.workspaceFolders?.[0]?.uri?.fsPath ?? "";
 }
